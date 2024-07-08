186e2c87d38c736d9d5b4de56705dec5f491a64d
zeekhoks <106755622+zeekhoks@users.noreply.github.com>
2024-07-08 18:43:00 -0400

    fix: added ui search error

--- src/components/BooksApp/BooksApp.tsx
+++ src/components/BooksApp/BooksApp.tsx
@@ -3,13 +3,16 @@ import { useState } from "react";
 import RTable from "../RTable/RTable";
 import SearchBar from "../Search/Search";
 import Container from 'react-bootstrap/Container';
+import Alert from 'react-bootstrap/Alert';
 import './booksapp.css';
 
 const BookApp = () => {
     const [searchTerm, setSearchTerm] = useState<string>('');
+    const [error, setError] = useState<boolean>(false);
 
     const getDataFromChild = (data: string) => {
         setSearchTerm(data);
+        if (searchTerm.length < 3) setError(true);
     }
 
     return (
@@ -21,7 +24,10 @@ const BookApp = () => {
                         <h2>Welcome to The Books Nook</h2>
                         <span>Your personal corner for your favorite books!</span>
                     </div>
+
                     <SearchBar sendDataToParent={getDataFromChild} />
+                    
+                    {error && <p className='error'>Please enter a search term greater than 3 characters.</p>}
                 </div>
             </div>
             {searchTerm.length > 3 ? <RTable searchTerm={searchTerm} /> : <></>}
